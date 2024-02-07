422444b9fed494d2bf7ac0d530d585745cf41157
khs <parts1123@gmail.com>
2024-02-07 11:07:24 +0900

    khs/ 가이드 작성> graft 정리

--- docs/plain/src/front-end/graft-js.js
+++ docs/plain/src/front-end/graft-js.js
@@ -25,40 +25,12 @@ app.exec(_ =>{
             app.markdown.fetchFiles(...[
                 '../resource/markdown/graft/graft-getstart.md',
                 '../resource/markdown/graft/graft-api.md',
-                '../resource/markdown/graft/graft-api-init.md',
-                '../resource/markdown/graft/graft-api-exec.md',
-                '../resource/markdown/graft/graft-api-tag.md',
-                '../resource/markdown/graft/graft-api-config.md',
-                '../resource/markdown/graft/graft-api-log.md',
-                '../resource/markdown/graft/graft-api-resource.md',
-                '../resource/markdown/graft/graft-api-imports.md',
-                '../resource/markdown/graft/graft-api-template.md',
-                '../resource/markdown/graft/graft-api-view.md',
-                '../resource/markdown/graft/graft-api-html.md',
-                '../resource/markdown/graft/graft-api-utils.md',
-                '../resource/markdown/graft/graft-api-router.md',
-                '../resource/markdown/graft/graft-api-bridge.md',
                 '../resource/markdown/graft/graft-tag.md',
                 '../resource/markdown/graft/graft-bridge.md',
             ])
                 .then(texts =>{
                     app.markdown.render(vo.getstart, texts.shift());
-
                     app.markdown.render(vo.api, texts.shift());
-                    app.markdown.render(vo.apiInit, texts.shift());
-                    app.markdown.render(vo.apiExec, texts.shift());
-                    app.markdown.render(vo.apiTag, texts.shift());
-                    app.markdown.render(vo.apiConfig, texts.shift());
-                    app.markdown.render(vo.apiLog, texts.shift());
-                    app.markdown.render(vo.apiResource, texts.shift());
-                    app.markdown.render(vo.apiImports, texts.shift());
-                    app.markdown.render(vo.apiTemplate, texts.shift());
-                    app.markdown.render(vo.apiView, texts.shift());
-                    app.markdown.render(vo.apiHtml, texts.shift());
-                    app.markdown.render(vo.apiUtils, texts.shift());
-                    app.markdown.render(vo.apiRouter, texts.shift());
-                    app.markdown.render(vo.apiBridge, texts.shift());
-
                     app.markdown.render(vo.tag, texts.shift());
                     app.markdown.render(vo.bridge, texts.shift());
                 });
